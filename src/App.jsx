0584942c6a8f1234688d51bd8c9326db61029fa5
vidakpop <daviskpop@gmail.com>
2025-03-18 13:03:58 +0300

    add HeroSection component to App for enhanced user experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
 
 const App = () => {
   return (
     <div className='bg-gray-50 text-gray-900'> 
     
       <Navbar />
+      <HeroSection />
       
       
 
